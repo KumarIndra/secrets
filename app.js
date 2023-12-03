21e1a26c87192ae901bda301b5300bd3266ad4e3
KumarIndra <123indrakumar@gmail.com>
2023-12-03 16:28:22 +0530

    LEVEL 5 - OAUTH Authentication

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import session from 'express-session';
+import passport from 'passport';
+import oauth2 from 'passport-google-oauth2';
+
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
+//Session 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+app.use(passport.initialize());
+app.use(passport.session());
 
 // DATABASE CONNECTION
 const db = new pg.Client({
@@ -23,6 +36,29 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+const GoogleStrategy = oauth2.Strategy;
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    // console.log(profile.id);
+    return done(null, profile);
+  }
+));
+
+passport.serializeUser((user, done)=>{
+    done(null,user);
+});
+
+passport.deserializeUser((user, done)=>{
+    done(null,user);
+});
+
+
 app.get("/", (req,res)=>{
     try {
         res.render("home");
@@ -30,6 +66,36 @@ app.get("/", (req,res)=>{
         console.log(error);
     }
 });
+
+app.get("/auth/google", 
+    passport.authenticate("google", { scope: ['email','profile']})
+);
+
+app.get("/auth/google/callback", 
+    passport.authenticate("google", {
+        failureRedirect: "/login"
+    }), (req,res) => {
+        //Successful authentication, redirect to secrets.
+        res.redirect("/secrets");
+    }
+);
+
+app.get("/secrets", (req,res)=>{
+    if(req.user){
+        res.render("secrets");
+    }else{
+        res.redirect("/");
+    }
+});
+
+app.get("/logout", function(req, res, next){
+    req.logout((err)=>{
+        if (err){
+            return next(err);
+        }
+        res.redirect("/");
+    });
+  });
 
 app.get("/register", (req,res)=>{
     res.render("register");
